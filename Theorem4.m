bae09fe1f877081f5516ffded25a163c016a1a5d
110500617 <110500617@163.com>
2024-06-18 10:57:35 +0800

    Create Theorem4.m

---
+++ Theorem4.m
@@ -0,0 +1,22 @@
+t=0:1/100:(1-1/100);
+x=sin(2*pi*25*t);
+h=sin(2*pi*10*t);
+
+
+C_xt=zeros(size(x,2),size(x,2));
+C_xt(1,:)=x;
+for i=1:size(x,2)-1
+      C_xt(i+1,:)=circshift(x,i);     
+end
+figure;
+plot(t,C_xt*h.','g','LineWidth',2)
+xlabel('Time(s)');
+ylabel('Amplitude');
+set(gca,'FontWeight','bold','fontweight','b');
+hold on;
+figure;
+plot(t,real(ifft(conj(fft(x)).*fft(h))),'r','LineWidth',2)
+xlabel('Time(s)');
+ylabel('Amplitude');
+set(gca,'FontWeight','bold','fontweight','b');
+hold on
